98181fe3a413576d42355f4def225559dae8bc7a
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-04 22:11:35 +0530

    mainContent/dashboard  setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header';
 import Asidebar from './components/Asidebar';
+import MainContent from './components/MainContent'
 function App() {
   return (
     <div className="App">
     <Header />
     <Asidebar />
+    <MainContent />    
     </div>
   );
 }
