97de5e7e0047411a2fd5bdec9e21bb18a178f62e
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-10-05 22:31:04 +0530

    getting data from api and show in table

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Read = () => {
+  const [getdata, setData] = useState([]);
+
+  const getuser = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setData(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getuser();
+  }, []);
+
   return (
     <>
-    <main id="main" className="Main">
-    <div className="pagetitle">
+      <main id="main" className="Main">
+        <div className="pagetitle">
           <h1>Read Data Page</h1>
           <nav>
             <ol className="breadcrumb">
@@ -17,10 +34,28 @@ const Read = () => {
           </nav>
         </div>
         <h1> You Are On The Read Me Page</h1>
-     
-    </main>
-    </>
-  )
-}
 
-export default Read
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          {getdata.map((data) => (
+            <tbody key={data.id}>
+              <tr>
+                <td>{data.id} </td>
+                <td>{data.name} </td>
+                <td>{data.email} </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </main>
+    </>
+  );
+};
+
+export default Read;
