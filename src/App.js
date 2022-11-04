46bd31823142e6e6774d54e93eddb94e2a8cfd79
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-11-04 23:25:24 +0530

    code format

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import Header from './components/header';
-import Asidebar from './components/Asidebar';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import { BrowserRouter , Routes,Route } from "react-router-dom";
-import Read from './components/Read';
-import Create from './components/create';
+import "./App.css";
+import Header from "./components/header";
+import Asidebar from "./components/Asidebar";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Read from "./components/Read";
+import Create from "./components/create";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+      <BrowserRouter>
         <Header />
         <Asidebar />
-           <Routes>
-           <Route path="/" element={<MainContent /> }></Route>
-           <Route path="/read" element={<Read /> }></Route>
-           <Route path="/create" element={<Create/> }></Route>                             
-           </Routes>
+        <Routes>
+          <Route path="/" element={<MainContent />}></Route>
+          <Route path="/read" element={<Read />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+        </Routes>
         <Footer />
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
