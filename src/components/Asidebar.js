0b806f7ff49e8c36cba124da9558cf9d9901ca0c
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-02-17 03:58:52 +0530

    correction

--- src/components/Asidebar.js
+++ src/components/Asidebar.js
@@ -6,7 +6,7 @@ const asidebar = () => {
     <>
       <aside id="sidebar" className="sidebar">
         <ul className="sidebar-nav" id="sidebar-nav">
-          <li className="nav-item">g
+          <li className="nav-item">
             <NavLink className="nav-link" to="/">
               <i className="bi bi-grid"></i>Dashboard
             </NavLink>
